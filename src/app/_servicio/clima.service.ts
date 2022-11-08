0aa4725f475f3773bf6f6a29b9b55114d1801d1a
alejandro espinosa <aespinosa@labsxd.com>
2022-11-08 20:29:59 -0300

    se agrego la key

--- src/app/_servicio/clima.service.ts
+++ src/app/_servicio/clima.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+ 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClimaService {
 
-  constructor() { }
+  key = 'aa1756cfebf4cd0a29d5c4478493402e';
+
+  // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+  constructor(private http: HttpClient) { }
+  
+  clima(ciudad: string): Observable<any>{
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&mode=json&units=metric&appid=${this.key}`);
+  }
+
 }
