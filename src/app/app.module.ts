5c80ea678dafaaef75b1b01d51cf2fc4ab78cf0f
alejandro espinosa <aespinosa@labsxd.com>
2022-11-08 22:24:38 -0300

    bugfix en la llamada al metodo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
